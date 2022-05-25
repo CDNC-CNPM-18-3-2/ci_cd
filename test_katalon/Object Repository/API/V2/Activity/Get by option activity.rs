02c340d92e356bd0fdeeb029008c7aae3110db49
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2022-05-25 20:38:28 +0700

    Add get by type test case

--- test_katalon/Object Repository/API/V2/Activity/Get by option activity.rs
+++ test_katalon/Object Repository/API/V2/Activity/Get by option activity.rs
@@ -6,13 +6,13 @@
    <elementGuidId>18cb8454-d82e-4ac8-83ad-da78c5bf5c50</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
-   <connectionTimeout>-1</connectionTimeout>
+   <connectionTimeout>0</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent></httpBodyContent>
    <httpBodyType></httpBodyType>
    <katalonVersion>8.3.0</katalonVersion>
-   <maxResponseSize>-1</maxResponseSize>
+   <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>GET</restRequestMethod>
    <restUrl>${GlobalVariable.HOST_URL}/api/activity?key=${key}&amp;participants=${participants}&amp;price=${price}&amp;minprice=${minprice}&amp;maxprice=${maxprice}&amp;accessibility=${accessibility}&amp;minaccessibility=${minaccessibility}&amp;maxaccessibility=${maxaccessibility}</restUrl>
@@ -22,7 +22,7 @@
    <soapRequestMethod></soapRequestMethod>
    <soapServiceEndpoint></soapServiceEndpoint>
    <soapServiceFunction></soapServiceFunction>
-   <socketTimeout>-1</socketTimeout>
+   <socketTimeout>0</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
    <variables>
       <defaultValue>''</defaultValue>
--- test_katalon/Test Suites/TS_Staging.ts
+++ test_katalon/Test Suites/TS_Staging.ts
@@ -22,17 +22,10 @@
       <guid>0db72792-889a-4ddd-be48-b4de39f49e08</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <iterationNameVariable>
-         <defaultValue>'5881028'</defaultValue>
-         <description></description>
-         <id>0a5c3130-d218-45e3-bb21-7fac8dbc3bdf</id>
-         <masked>false</masked>
-         <name>key</name>
-      </iterationNameVariable>
       <testCaseId>Test Cases/API/V2/Activity/Get by key activity</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>c0e42661-59c1-4e5c-b06b-63da680b616a</id>
+         <id>02127df8-646f-42b9-b58c-b2dce70551e5</id>
          <iterationEntity>
             <iterationType>ALL</iterationType>
             <value></value>
@@ -41,45 +34,103 @@
       </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>key</value>
          <variableId>0a5c3130-d218-45e3-bb21-7fac8dbc3bdf</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>activity</value>
          <variableId>cba8ed18-9334-4aeb-b300-a2b4e11f2707</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>type</value>
          <variableId>092c01d6-1a7c-404f-94f7-9744b9f4bc7c</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>participants</value>
          <variableId>743593a8-3714-46c7-9991-06c3209f755b</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>price</value>
          <variableId>33a45203-1756-43b1-918b-9e4a2751bad3</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>link</value>
          <variableId>c497df4b-ecbb-48d9-8457-0530c31fe5f0</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>c0e42661-59c1-4e5c-b06b-63da680b616a</testDataLinkId>
+         <testDataLinkId>02127df8-646f-42b9-b58c-b2dce70551e5</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>accessibility</value>
+         <variableId>a9a2b63f-29d8-42ec-acd4-8a5880498256</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5a50ef7-3c22-4677-8aaf-fdb55c740a19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/V2/Activity/Get by type</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>696cec85-89d7-4b6e-b500-3ff72b1dc592</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Activity/Data activity</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0a5c3130-d218-45e3-bb21-7fac8dbc3bdf</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>cba8ed18-9334-4aeb-b300-a2b4e11f2707</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>696cec85-89d7-4b6e-b500-3ff72b1dc592</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>type</value>
+         <variableId>092c01d6-1a7c-404f-94f7-9744b9f4bc7c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>743593a8-3714-46c7-9991-06c3209f755b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>33a45203-1756-43b1-918b-9e4a2751bad3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c497df4b-ecbb-48d9-8457-0530c31fe5f0</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
          <variableId>a9a2b63f-29d8-42ec-acd4-8a5880498256</variableId>
       </variableLink>
    </testCaseLink>
